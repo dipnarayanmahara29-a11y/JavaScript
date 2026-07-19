9dec0462a29e28476c1ce9aa9b273687a2518433
Dip Narayan Mahara <dipnarayanmahara28@gmail.com>
2026-07-19 21:00:36 +0545

    Day 1: Learing variable, Data type and Operators

---
+++ Day1/lesson1.js
@@ -0,0 +1,28 @@
+// Lesson 1:
+// Variable : Variable is a named or labled container which stores the values.
+
+name = "alice"; // This is the example of variable where name is a variable and alice is the value stored in the name variable.
+
+console.log(name);
+
+// Data Type : Data type means what type of value is stored in the variable such as string, number, or boolean (true or false).
+
+// Exercise 1:
+const firstName = "Dip Narayan Mahara"; // const is a declaration of the variable which means this value cannot be changed.
+let age = 23; // let means the value can be change during any process
+const isStudent = true;
+
+const country = "Nepal";
+
+console.log(firstName);
+console.log(age);
+console.log(isStudent);
+console.log(country);
+
+age = age + 1;
+console.log(age);
+
+console.log(typeof firstName);
+console.log(typeof age);
+console.log(typeof isStudent);
+console.log(typeof country);
---
+++ Day1/lesson2.js
@@ -0,0 +1,42 @@
+// // Lesson 2 : Operators
+// // ----> Operators is a symbols that tells JavaScript to perform an operations on one or more valuse. Symbols like "+", "-", "/" or "*" and many more.
+
+// // ----> there are different types of operators like:
+// // Arithematic Operators
+// // Assignment Operators
+// // Increament and Decrement Operators
+
+// // Exercise
+
+// let a = 15; // "=" this sign is the assignment operators which assign the value "15" to the variable a
+// let b = 4;
+
+// // Here is all the arithematic Operators
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a / b);
+// console.log(a * b);
+// console.log(a % b);
+// console.log(a ** b);
+
+// let score = 20;
+
+// // This is also an assignment operators
+// score += 10;
+// score -= 5;
+// score *= 2;
+// score /= 5;
+
+// console.log(score);
+
+let y = 8;
+
+console.log(++y);
+
+console.log(y);
+
+let x = 8;
+
+console.log(x++);
+
+console.log(x);
